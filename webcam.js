46a00e727c7cdefe5058cb2b9b56cf9ee11445ea
SabrinaCat <Sabrina.glow22@gmail.com>
2020-04-01 10:32:25 -0500

    dropdown menu for country select

---
+++ webcam.js
@@ -0,0 +1,20 @@
+//var myKey= "
+
+/*$.ajax({
+    url: "https://api.windy.com/api/webcams/v2/country={unitedstates}" + "?key=eMD8jddw0zG16xeTQeGWTryCpN6pf6R5",
+    method: 'GET',
+}
+).then(function(data){
+    console.log(data);
+});*/
+
+
+/*var userInput= "select contry from drop down, or user input or select list"
+$.ajax({
+    url: "https://api.windy.com/api/webcams/v2/list?show=countries&key=eMD8jddw0zG16xeTQeGWTryCpN6pf6R5",
+    method: 'GET',
+}
+).then(function(data){
+    console.log(data)
+});*/
+
