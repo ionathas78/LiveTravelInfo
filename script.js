29f4fbf9192f65b7a42c07307ec332bff1cc4e77
Michael Cassaro <mdcassaro@hotmail.com>
2020-04-02 14:20:19 -0500

    exchange js

---
+++ script.js
@@ -0,0 +1,97 @@
+// GET https://prime.exchangerate-api.com/v5/YOUR-API-KEY/latest/USD
+// APIkey: 09e9c25cfea5c28f56798f1c
+
+{
+	"result": "success",
+	"documentation": "https://www.exchangerate-api.com/docs",
+	"terms_of_use": "https://www.exchangerate-api.com/terms",
+	"time_zone": "UTC",
+	"time_last_update": 1580947200,
+	"time_next_update": 1580950860,
+	"base": "USD",
+	"conversion_rates": {
+		"USD": 1,
+		"AUD": 1.4817,
+		"BGN": 1.7741,
+		"CAD": 1.3168,
+		"CHF": 0.9774,
+		"CNY": 6.9454,
+		"EGP": 15.7361,
+		"EUR": 0.9013,
+		"GBP": 0.7679,
+		"...": 7.8536,
+		"...": 1.3127,
+		"...": 7.4722, etc. etc.
+	}
+}
+
+// Currency Code	Currency Name	Country
+// AED	UAE Dirham	United Arab Emirates
+// ARS	Argentine Peso	Argentina
+// AUD	Australian Dollar	Australia
+// BGN	Bulgarian Lev	Bulgaria
+// BRL	Brazilian Real	Brazil
+// BSD	Bahamian Dollar	Bahamas
+// CAD	Canadian Dollar	Canada
+// CHF	Swiss Franc	Switzerland
+// CLP	Chilean Peso	Chile
+// CNY	Chinese Renminbi	China
+// COP	Colombian Peso	Colombia
+// CZK	Czech Koruna	Czech Republic
+// DKK	Danish Krone	Denmark
+// DOP	Dominican Peso	Dominican Republic
+// EGP	Egyptian Pound	Egypt
+// EUR	Euro	Germany
+// EUR	Euro	Austria
+// EUR	Euro	Belgium
+// EUR	Euro	Cyprus
+// EUR	Euro	Estonia
+// EUR	Euro	Finland
+// EUR	Euro	France
+// EUR	Euro	Greece
+// EUR	Euro	Ireland
+// EUR	Euro	Italy
+// EUR	Euro	Latvia
+// EUR	Euro	Lithuania
+// EUR	Euro	Luxembourg
+// EUR	Euro	Malta
+// EUR	Euro	Netherlands
+// EUR	Euro	Portugal
+// EUR	Euro	Slovakia
+// EUR	Euro	Slovenia
+// EUR	Euro	Spain
+// FJD	Fiji Dollar	Fiji
+// GBP	Pound Sterling	United Kingdom
+// GTQ	Guatemalan Quetzal	Guatemala
+// HKD	Hong Kong Dollar	Hong Kong
+// HRK	Croatian Kuna	Croatia
+// HUF	Hungarian Forint	Hungary
+// IDR	Indonesian Rupiah	Indonesia
+// ILS	Israeli New Shekel	Israel
+// INR	Indian Rupee	India
+// ISK	Icelandic Krona	Iceland
+// JPY	Japanese Yen	Japan
+// KRW	South Korean Won	South Korea
+// KZT	Kazakhstani Tenge	Kazakhstan
+// MXN	Mexican Peso	Mexico
+// MYR	Malaysian Ringgit	Malaysia
+// NOK	Norwegian Krone	Norway
+// NZD	New Zealand Dollar	New Zealand
+// PAB	Panamanian Balboa	Panama
+// PEN	Peruvian Sol	Peru
+// PHP	Philippine Peso	Philippines
+// PKR	Pakistani Rupee	Pakistan
+// PLN	Polish Zloty	Poland
+// PYG	Paraguayan Guarani	Paraguay
+// RON	Romanian Leu	Romania
+// RUB	Russian Ruble	Russia
+// SAR	Saudi Riyal	Saudi Arabia
+// SEK	Swedish Krona	Sweden
+// SGD	Singapore Dollar	Singapore
+// THB	Thai Baht	Thailand
+// TRY	Turkish Lira	Turkey
+// TWD	New Taiwan Dollar	Taiwan
+// UAH	Ukrainian Hryvnia	Ukraine
+// USD	United States Dollar	United States
+// UYU	Uruguayan Peso	Uruguay
+// ZAR	South African Rand	South Africa
