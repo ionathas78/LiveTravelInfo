3af9d3513864ed63544b47b4a1ebbc0853137fec
Jonathan Andrews <ionathas78@hotmail.com>
2020-04-03 13:33:58 -0500

    Fuzzy name matching algorithm added

--- iataDBase.js
+++ iataDBase.js
@@ -3,6 +3,7 @@
 const _TEST_CITYNAME = "Austin";
 const _TEST_COUNTRYCODE = "";
 const _DEFAULT_COUNTRYCODE = "US";
+const _FUZZYMATCH_THRESHOLD = 0.65;
 
 // const _CORS_SERVER = "https://polar-bayou-73801.herokuapp.com/";
 // const citiesURL = "https://github.com/SabrinaCat/LiveTravelInfo/blob/master/Assets/cities.json";
@@ -32,9 +33,67 @@ function returnCityInfo(cityName, countryCode) {
         };
     };
 
+    if (!returnObject) {
+        let matchCity;
+        let matchPercent = 0;
+        for (var fuzzyCity of citiesData) {
+            if ((countryCode == fuzzyCity.country_code) || (countryCode == "")) {
+                let testPercent = textPercentMatch(cityName, fuzzyCity.name)
+
+                if (testPercent > matchPercent) {
+                    matchPercent = testPercent;
+                    matchCity = fuzzyCity;
+                };
+            };
+        };
+
+        if (matchPercent >= _FUZZYMATCH_THRESHOLD) {
+            returnObject = new CityData(matchCity.name, matchCity.country_code, matchCity.code, matchCity.coordinates.lat, matchCity.coordinates.lon, matchCity.time_zone);
+        };
+    };
+
     // let endTime = Date.now();
     // console.log("Lookup operation took " + (endTime - startTime) + " seconds.");
     return returnObject;
+};
+
+function returnCountryCode (countryName) {
+    let returnString = "";
+
+    for (var country of countriesData) {
+        if (country.name == countryName) {
+            returnString = country.code;
+            break;
+        };
+    };
+
+    if (returnString == "") {
+        for (var country of countriesData) {
+            if (country.code == countryName) {
+                returnString = countryName;
+                break;
+            };
+        };
+    };
+
+    if (returnString == "") {
+        let matchCode = "";
+        let matchPercent = 0;
+
+        for (var country of countriesData) {
+            let testPercent = textPercentMatch(countryName, country.name);
+            if (testPercent > matchPercent) {
+                matchPercent = testPercent;
+                matchCode = country.code;
+            };
+        };
+
+        if (matchPercent >= _FUZZYMATCH_THRESHOLD) {
+            returnString = matchCode;
+        }
+    };
+
+    return returnString;
 };
 
 /**
@@ -160,7 +219,7 @@ function runCitySearch() {
 
     if (commaPos > -1) {
         userCity = userInput.substring(0, commaPos).trim();
-        userCountry = userInput.substring(commaPos + 1).trim();
+        userCountry = returnCountryCode(userInput.substring(commaPos + 1).trim());
     } else {
         userCity = userInput.trim();
     }
@@ -168,6 +227,86 @@ function runCitySearch() {
     _queryStart = new Date();
     renderCity(userCity, userCountry);
 }
+
+/**
+ * Compares two strings and returns the percentage of similarity
+ * @param {Number} txt1 First string to compare
+ * @param {Number} txt2 Second string to compare
+ * This algorithm copyright 2015 by Jonathan Andrews; usage per MIT license.
+ */
+function textPercentMatch (txt1, txt2) {
+    let returnPercent = -1;
+
+    if (txt1 == txt2) {
+        returnPercent = 1;
+
+    } else {
+        let longString, shortString;
+
+        if (txt1.length >= txt2.length) {
+            longString = txt1;
+            shortString = txt2;
+        } else {
+            longString = txt2;
+            shortString = txt1;
+        };
+
+        let startPos = 0;
+        let charactersMatched = 0;
+        let endPosShort = shortString.length - 1;
+        let resultIncrement = 0.995 / (longString.length - countString(longString, " "));
+            
+        while (startPos <= endPosShort) {
+            let matchString = "";
+            let matchLength = 0;
+            let significantLength = 0;
+            
+            do {
+                matchLength++;
+                matchString = shortString.substr(startPos, matchLength);
+            } while ((startPos + matchLength <= endPosShort) && (longString.indexOf(matchString) > -1));
+            if (startPos + matchLength < endPosShort) {
+                matchLength--;
+            };
+            significantLength = matchLength - countString(matchString, " ");
+
+            if (significantLength > 1) {
+                charactersMatched += significantLength;
+                matchString = matchString.substr(0, matchLength);
+                longString = longString.replace(matchString, "");
+            };
+            if (matchLength > 1) {
+                startPos += matchLength;
+            } else {
+                startPos++;
+            };
+        };
+
+        returnPercent = charactersMatched * resultIncrement;
+    };
+
+    return returnPercent;
+};
+
+/**
+ * Counts the instances of given string inside another string
+ * @param {Text} baseText Text in which to search for the search string
+ * @param {Text} searchText Text for which to search
+ */
+function countString(baseText, searchText) {
+    let returnCount = 0;
+    let characterPos = 0;
+    let endPos = baseText.length - 1;
+
+    do {
+        characterPos = baseText.indexOf(searchText, characterPos + 1);
+        if (characterPos > 0) {
+            returnCount++;
+        };
+    } while ((characterPos > -1) && (characterPos < endPos));
+
+    return returnCount;
+};
 
 // _request.onload = function() {
 //     _citiesDB = _request.response;
@@ -194,3 +333,6 @@ function runCitySearch() {
 
 
 // testCityDB();
+
+// console.log(countString("Hello World!", "e"));
+// console.log(textPercentMatch("Mexico City, Mexico", "Mehico City, Mehico"));
